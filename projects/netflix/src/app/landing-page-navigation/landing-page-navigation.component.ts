0e9d1ab35160c450fa0474b2c6735cff4c727ff1
riaz <riazasim111@gmail.com>
2021-05-04 21:06:29 +0200

    landing page navigation component created

---
+++ projects/netflix/src/app/landing-page-navigation/landing-page-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-navigation',
+  templateUrl: './landing-page-navigation.component.html',
+  styleUrls: ['./landing-page-navigation.component.scss']
+})
+export class LandingPageNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/netflix/src/app/landing-page/landing-page.module.ts
+++ projects/netflix/src/app/landing-page/landing-page.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingPageNavigationComponent } from '../landing-page-navigation/landing-page-navigation.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageNavigationComponent
+  ],
   imports: [
     CommonModule
   ]
