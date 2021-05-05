60e419aaa3efd99ae49e577502f2d30297c05646
riaz <riazasim111@gmail.com>
2021-05-05 20:38:08 +0200

    landing-page registration component created

---
+++ projects/netflix/src/app/landing-page/landing-page-registration/landing-page-registration.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-registration',
+  templateUrl: './landing-page-registration.component.html',
+  styleUrls: ['./landing-page-registration.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingPageRegistrationComponent {
+
+  constructor() { }
+
+
+}
