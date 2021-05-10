0b23bef335421c597e370fbe830d3debba640908
riaz <riazasim111@gmail.com>
2021-05-10 15:16:06 +0200

    animation modified

--- projects/netflix/src/app/landing-page/registration-animation.ts
+++ projects/netflix/src/app/landing-page/registration-animation.ts
@@ -5,14 +5,15 @@ import { trigger, transition, style, animate, keyframes } from '@angular/animati
 
 export const registrationAnimation = trigger('registrationAnimation', [
   transition(':enter', [
-    animate('1000ms', keyframes([
-      style({opacity: 0, transform: 'translateY(50%)'}),
-      style({opacity: 0.2, transform: 'translateY(40%)'}),
-      style({opacity: 0.4, transform: 'translateY(30%)'}),
-      style({opacity: 0.6, transform: 'translateY(20%)'}),
-      style({opacity: 0.8, transform: 'translateY(10%)'}),
-      style({opacity: 1, transform: 'translateY(0%)'}),
-    ]))
+    style({
+      opacity: 0,
+      transform: 'translateY(50%)'
+    }),
+
+    animate('700ms', style({
+      opacity: 1,
+      transform: 'translateY(0)'
+    }))
       
   ])
      
