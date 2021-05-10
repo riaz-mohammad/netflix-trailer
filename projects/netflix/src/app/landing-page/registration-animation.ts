5fa3fdb4c81f1c66a7bb8621f43f00b5c1c14953
riaz <riazasim111@gmail.com>
2021-05-10 10:15:24 +0200

    registration animation added

--- projects/netflix/src/app/landing-page/registration-animation.ts
+++ projects/netflix/src/app/landing-page/registration-animation.ts
@@ -7,7 +7,7 @@ export const registrationAnimation = trigger('registrationAnimation', [
   transition(':enter', [
     style({
       opacity: 0,
-      transform: 'translateY(50%) scale(0.8)',
+      transform: 'translateY(20%) scale(0.8)',
     }),
 
     animate('1000ms', style({
