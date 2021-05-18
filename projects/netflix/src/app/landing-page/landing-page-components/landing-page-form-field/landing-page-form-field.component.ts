e26899609d07141cea6300711cb7a2490a32b6d3
riaz <riazasim111@gmail.com>
2021-05-18 23:34:05 +0200

    form field component created

---
+++ projects/netflix/src/app/landing-page/landing-page-components/landing-page-form-field/landing-page-form-field.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-form-field',
+  templateUrl: './landing-page-form-field.component.html',
+  styleUrls: ['./landing-page-form-field.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingPageFormFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
