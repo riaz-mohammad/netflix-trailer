88f2e523c287e4633888d4e9440519fd09c037fc
riaz <riazasim111@gmail.com>
2021-05-18 11:38:45 +0200

    some tweaking

--- projects/netflix/src/app/app-routing.module.ts
+++ projects/netflix/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageMainGridComponent } from './landing-page/landing-page-components/landing-page-main-grid/landing-page-main-grid.component';
+
 
 
 
 const routes: Routes = [
+  {
+    path: '', component: LandingPageMainGridComponent
+  },
   {
     path: '', pathMatch: 'full', redirectTo: ''
   },
