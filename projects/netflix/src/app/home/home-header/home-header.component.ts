6150118c74f383374bf3d60ebea1eec6da568d13
riaz <riazasim111@gmail.com>
2021-05-25 15:03:31 +0200

    http functionality added

--- projects/netflix/src/app/home/home-header/home-header.component.ts
+++ projects/netflix/src/app/home/home-header/home-header.component.ts
@@ -1,25 +1,39 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
-import { MOVIES, MoviesService } from './../movies.service';
+import { MoviesService, Movie, } from './../movies.service';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, AfterViewInit } from '@angular/core';
+
+import { URLS, IMAGES } from './../urls';
+
+
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeHeaderComponent implements OnInit {
- 
-  constructor(public movieService: MoviesService) { }
-  public imageUrl!: string;
-  public data!: Array<any>;
+  constructor(private moviesService: MoviesService) {}
+  public images = IMAGES;
+  public data = this.moviesService.getTrending();
+
   ngOnInit(): void {
-    console.log('Initiated...')
-   
+    console.log('Initiated...');
+    this.data.subscribe(console.log);
+  }
+  public getMovieInfo(movie: Movie): void {
+    let trailer: Observable<any> = this.moviesService.getMovieTrailer(movie.id);
+    trailer.subscribe(console.log);
+    
+    
   }
     
-    
+  
+
+
 }
+    
+    
     
       
 
--- projects/netflix/src/app/home/movies.service.ts
+++ projects/netflix/src/app/home/movies.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { API_KEY, BASE_URL, URLS } from './urls';
 
-export interface MOVIES  {
+export interface Movie  {
   genre_ids: Array<number>;
   id: number;
   title: string;
@@ -13,45 +15,36 @@ export interface MOVIES  {
 }
 
 interface Results {
-  results: MOVIES[];
+  results: Movie[];
+}
+
+
+export interface Key {
+  key: string;
+}
+ 
+
+export interface MovieTrailer {
+  results: Array<any>;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-  private readonly NETFLIX_URL = 'https://api.themoviedb.org/3';
-  private readonly API_KEY = '214849890feefa84c31c1ba22d9ef072';
-  private readonly TRENDING = `${this.NETFLIX_URL}/trending/all/week?api_key=${this.API_KEY}`;
-  private readonly MOVIE_TRAILER = `${this.NETFLIX_URL}/movie/${123}/videos?api_key=${this.API_KEY}`;
-  private readonly SHOW_TRAILER = `${this.NETFLIX_URL}/tv/${123}/videos?api_key=${this.API_KEY}`;
-  private readonly MOVIE_INFO = `${this.NETFLIX_URL}/movie/${123}?api_key=${this.API_KEY}`;
-  private readonly SIMILAR_MOVIE = `${this.NETFLIX_URL}/movie/${123}/similar?api_key=${this.API_KEY}`;
-  private readonly SIMILAR_SHOW = `${this.NETFLIX_URL}/tv/{tv_id}/similar?api_key=${this.API_KEY}page=1`;
-  private readonly GENRE_MOVIES = `${this.NETFLIX_URL}/genre/movie/list?api_key=${this.API_KEY}`;
-  private readonly GENRE_SHOWS = `${this.NETFLIX_URL}/genre/tv/list?api_key=${this.API_KEY}`;
-  private readonly GENRE_MOVIES_SPECIFIC = `${this.NETFLIX_URL}/discover/movie?api_key=${this.API_KEY}&with_genres=28`;
-  private readonly GENRE_SHOWS_SPECIFIC = `${this.NETFLIX_URL}/discover/tv?api_key=${this.API_KEY}&with_genres=28`;
-  private readonly POPULAR_MOVIES = `${this.NETFLIX_URL}/movie/popular?api_key=${this.API_KEY}&page=1`;
-  private readonly GET_MOVIE_BY_NAME = `${this.NETFLIX_URL}/search/movie?api_key=${this.API_KEY}&query=${'MOVIE NAME'}`;
-  private readonly GET_SHOW_BY_NAME = `${this.NETFLIX_URL}/search/tv?api_key=${this.API_KEY}&query=${'SHOW NAME'}`;
-  private readonly SEARCH = `${this.NETFLIX_URL}/search/multi?api_key=${this.API_KEY}&query=${'SHOW OR MOVIE NAME'}`;
-  
-  public readonly IMAGES = 'https://image.tmdb.org/t/p/original/';
   constructor(private http: HttpClient) { }
   
-  public getMovies(): Observable<any> {
-    return of();
+  public getTrending(): Observable<Results> {
+    return this.http.get<Results>(URLS.TRENDING_URL);
+  }
+  
+  public getMovieTrailer(id: number): Observable<any> {
+    const MOVIE_TRAILER = `${BASE_URL}/movie/${id}/videos?api_key=${API_KEY}`;
+    return this.http.get<MovieTrailer>(MOVIE_TRAILER).pipe(
+      map(({ results }) => results?.[0] ?? 'Not Found'),
+      catchError((error) => of(error))
+    );
+      
   }
 
-  // return a movie/series/animation netflix's trailer from Youtube
-  public getTrailer(movieId: string): Observable<any> {
-    return of();
-  }
-
-  //return all the Trending Movies, Shows, Animation
-  public getTrending(): Observable<any> {
-      return this.http.get<any>(this.TRENDING);
-  }
-    
 }
