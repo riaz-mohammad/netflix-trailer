e0fa08503c6eb6868e6a2d96ddd06bb10ac37c77
riaz <riazasim111@gmail.com>
2021-06-04 00:51:03 +0200

    movies page http functionality added

--- projects/netflix/src/app/types/movies-genres.ts
+++ projects/netflix/src/app/types/movies-genres.ts
@@ -10,32 +10,24 @@ export const MOVIES_GENRES: MovieGenre[] =  [
       name: 'Adventure',
     },
     {
-      id: 16,
-      name: 'Animation',
-    },
-    {
-      id: 35,
-      name: 'Comedy',
-    },
-    {
       id: 80,
       name: 'Crime',
-    },
-    {
-      id: 99,
-      name: 'Documentary',
     },
     {
       id: 18,
       name: 'Drama',
     },
     {
+      id: 14,
+      name: 'Fantasy',
+    },
+    {
       id: 10751,
       name: 'Family',
     },
     {
-      id: 14,
-      name: 'Fantasy',
+      id: 99,
+      name: 'Documentary',
     },
     {
       id: 36,
@@ -44,10 +36,6 @@ export const MOVIES_GENRES: MovieGenre[] =  [
     {
       id: 27,
       name: 'Horror',
-    },
-    {
-      id: 10402,
-      name: 'Music',
     },
     {
       id: 9648,
@@ -60,6 +48,14 @@ export const MOVIES_GENRES: MovieGenre[] =  [
     {
       id: 878,
       name: 'Science Fiction',
+    },
+    {
+      id: 10402,
+      name: 'Music',
+    },
+    {
+      id: 35,
+      name: 'Comedy',
     },
     {
       id: 10770,
@@ -76,5 +72,9 @@ export const MOVIES_GENRES: MovieGenre[] =  [
     {
       id: 37,
       name: 'Western',
+    },
+    {
+      id: 16,
+      name: 'Animation',
     },
   ]
